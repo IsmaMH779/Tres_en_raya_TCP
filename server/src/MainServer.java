fd9ee61e2f1a1b58897d94f52c370fb1ed8596fa
ismael <ismaelmh08@gmail.com>
2025-01-15 11:44:01 +0100

    Servidor Tcp implementado (faltan detalles)

---
+++ server/src/MainServer.java
@@ -0,0 +1,51 @@
+import game.Match;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MainServer {
+
+    int port;
+    Match match;
+
+    public MainServer(int port) {
+        this.port = port;
+        this.match = new Match();
+    }
+
+    public  void listen() {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+
+        onInit();
+
+        try {
+            serverSocket = new ServerSocket(port);
+            // esperar conexion del cliente para lanzar el thread
+            while (true) {
+                clientSocket = serverSocket.accept();
+                InetAddress clientAdress = clientSocket.getInetAddress();
+                System.out.println( clientAdress + " connected");
+
+                // lanzar el thread y establecer la comunicacion
+                PlayerThread playerThread = new PlayerThread(clientSocket, match);
+                Thread player = new Thread(playerThread);
+                player.start();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void onInit() {
+        System.out.println("Servidor levantado, escuchando puerto: " + port);
+    }
+
+    public static void main(String[] args) {
+        MainServer srv = new MainServer(5558);
+        srv.listen();
+    }
+
+}
